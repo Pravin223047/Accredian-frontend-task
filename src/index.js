b811dd25281f827df1d5fa6a05a8b2b4a310271d
PRAVIN KSHIRSAGAR <107901482+Pravin223047@users.noreply.github.com>
2024-07-06 15:46:47 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router-dom";
 
 // Retrieve the Clerk Publishable Key from the environment variables
-const PUBLISHABLE_KEY = "pk_test_dW5pdGVkLWV3ZS01LmNsZXJrLmFjY291bnRzLmRldiQ";
+const PUBLISHABLE_KEY = "pk_test_cGV0LXR1cnRsZS03My5jbGVyay5hY2NvdW50cy5kZXYk";
 
 const container = document.getElementById("root");
 const root = createRoot(container);
